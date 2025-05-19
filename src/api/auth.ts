f03de1f3fd049c4892d012f739ef32244b9c850d
hamzalicious12 <hamza250.b@gmail.com>
2025-05-20 00:04:35 +0500

    Update auth.ts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AuthResponse } from '@/types';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://task-tracker-backend-qury.onrender.com';
 
 export const login = async (email: string, password: string): Promise<AuthResponse> => {
   try {
