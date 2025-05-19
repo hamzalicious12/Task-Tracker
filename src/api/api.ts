c5c16af75b12ebc450a1b29f7fa6c0a40a67f7cb
hamzalicious12 <hamza250.b@gmail.com>
2025-05-20 00:06:00 +0500

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', // Updated to match server port 5000
+  baseURL: import.meta.env.VITE_API_URL || 'https://task-tracker-backend-qury.onrender.com', // Updated to match server port 5000
   headers: {
     'Content-Type': 'application/json'
   },
